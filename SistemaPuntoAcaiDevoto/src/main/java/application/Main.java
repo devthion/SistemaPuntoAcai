8437ab0b74573e13959f363fccdf17681e755e9d
andycabrera <andycaabrera@users.noreply.github.com>
2020-05-15 19:42:00 -0300

    Diseñando la pantalla principal

--- SistemaPuntoAcaiDevoto/src/main/java/application/Main.java
+++ SistemaPuntoAcaiDevoto/src/main/java/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,900,500);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
