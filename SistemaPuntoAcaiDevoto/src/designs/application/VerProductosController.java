c53398c904fe10c6de1ccc54755fa1125cdaa533
andycabrera <andycaabrera@users.noreply.github.com>
2020-05-25 00:35:53 -0300

    Cambios en Agregar producto

--- SistemaPuntoAcaiDevoto/src/designs/application/VerProductosController.java
+++ SistemaPuntoAcaiDevoto/src/designs/application/VerProductosController.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class VerProductosController {
+public class VerProductosController implements Initializable {
 
     @FXML
     private Button btnActualizarStock;
@@ -12,6 +21,12 @@ public class VerProductosController {
     @FXML
     private Button btnAgregarNuevoProducto;
 
+    @FXML
+    private Button btnActualizarPrecio;
+    
+    @FXML
+    private Button btnVolver;
+    
     @FXML
     void onAgregarNuevoProductoClick(ActionEvent event) {
 
@@ -21,5 +36,35 @@ public class VerProductosController {
     void onActualizarStockClick(ActionEvent event) {
 
     }
+    
+    @FXML
+    void onActualizarPrecioClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onVolverClick(ActionEvent event) {
+    	try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("MenuPrincipal.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			Scene scene = new Scene(root,1300,650);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Menu Principal");
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    	Stage stage = (Stage) btnVolver.getScene().getWindow();
+    	stage.close();
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		//OBTENER LOS DATOS DE LOS PRODUCTOS
+		
+	}
 
 }
