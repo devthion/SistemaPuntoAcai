bc7b84a1efcd52a7ff190f10b3b8000ad7f5bf0f
andycabrera <andycaabrera@users.noreply.github.com>
2020-05-27 11:52:00 -0300

    [Ver Ventas] Falta la funcion obtener ventas en obtenerDatos

---
+++ SistemaPuntoAcaiDevoto/src/designs/application/VerVentasController.java
@@ -0,0 +1,89 @@
+package application;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+import ConexionBD.ObtenerDatos;
+import Ventas.Venta;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class VerVentasController implements Initializable{
+
+    @FXML
+    private TableColumn<Venta, LocalDate> colFecha;
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private TableColumn<Venta, Double> colGanancia;
+
+    @FXML
+    private TableView<Venta> tblVentas;
+
+    @FXML
+    private TableColumn<Venta, String> colCliente;
+
+    @FXML
+    private TableColumn<Venta, Double> colMontoTotal;
+    
+    private ObservableList<Venta> ventas;
+
+    @FXML
+    void onVolverClick(ActionEvent event) {
+    	try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("MenuPrincipal.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			Scene scene = new Scene(root,1300,650);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Menu Principal");
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    	Stage stage = (Stage) btnVolver.getScene().getWindow();
+    	stage.close();
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		ObtenerDatos obtenerDatos;
+		try {
+			obtenerDatos = new ObtenerDatos();
+			ventas = FXCollections.observableArrayList();
+			//ventas = obtenerDatos.obtenerVentas();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		this.tblVentas.setItems(ventas);
+		
+		this.colCliente.setCellValueFactory(new PropertyValueFactory<Venta, String>("nombre"));
+		this.colFecha.setCellValueFactory(new PropertyValueFactory<Venta, LocalDate>("costo"));
+		this.colGanancia.setCellValueFactory(new PropertyValueFactory<Venta, Double>("precioMayor"));
+		this.colMontoTotal.setCellValueFactory(new PropertyValueFactory<Venta, Double>("precioUnitario"));
+		
+	}
+
+}
