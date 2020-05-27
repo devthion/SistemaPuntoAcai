090c835ac17884fd14590b18c2178fa128e5c51e
andycabrera <andycaabrera@users.noreply.github.com>
2020-05-26 21:40:57 -0300

    Inicializando Nueva Venta

--- SistemaPuntoAcaiDevoto/src/designs/application/NuevaVentaController.java
+++ SistemaPuntoAcaiDevoto/src/designs/application/NuevaVentaController.java
@@ -1,14 +1,30 @@
 package application;
 
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import ConexionBD.ObtenerDatos;
+import ModelosClientes.Cliente;
+import Productos.Producto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class NuevaVentaController {
+public class NuevaVentaController implements Initializable {
 
     @FXML
     private TableColumn<?, ?> colClieDireccion;
@@ -23,37 +39,37 @@ public class NuevaVentaController {
     private Button btnNuevoCliente;
 
     @FXML
-    private TableView<?> tblProductosVenta;
+    private TableView<Producto> tblProductosVenta;
 
     @FXML
     private Button btnBuscarCliente;
 
     @FXML
-    private TableView<?> tblClientes;
+    private TableView<Cliente> tblClientes;
 
     @FXML
     private TableColumn<?, ?> colClieApellido;
 
     @FXML
-    private TableColumn<?, ?> colProdVentaCantidad;
+    private TableColumn<Integer, Integer> colProdVentaCantidad;
 
     @FXML
     private TableColumn<?, ?> colClieDni;
 
     @FXML
-    private TableColumn<?, ?> colProdKilos;
+    private TableColumn<Producto, Integer> colProdKilos;
 
     @FXML
-    private TableColumn<?, ?> colProdVentaPrecioTotal;
+    private TableColumn<Producto, Double> colProdVentaPrecioTotal;
 
     @FXML
     private TextField txtNombreABuscar;
 
     @FXML
-    private TableColumn<?, ?> colClieNombre;
+    private TableColumn<Cliente, String> colClieNombre;
 
     @FXML
-    private TableView<?> tblProductos;
+    private TableView<Producto> tblProductos;
 
     @FXML
     private Button btnMas;
@@ -62,10 +78,10 @@ public class NuevaVentaController {
     private Button btnVolver;
 
     @FXML
-    private TableColumn<?, ?> colProdNombre;
+    private TableColumn<Producto, String> colProdNombre;
 
     @FXML
-    private TableColumn<?, ?> colProdVentaNombre;
+    private TableColumn<Producto, String> colProdVentaNombre;
 
     @FXML
     private Button btnAgregarAlCarrito;
@@ -77,7 +93,11 @@ public class NuevaVentaController {
     private TextField txtPrecioTotal;
 
     @FXML
-    private TableColumn<?, ?> colProdStock;
+    private TableColumn<Producto, Integer> colProdStock;
+    
+    private int contadorCantidad;
+    private ObservableList<Producto> productos;
+    private ObservableList<Cliente> clientes;
 
     @FXML
     void onAgregarAlCarritoClick(ActionEvent event) {
@@ -111,7 +131,59 @@ public class NuevaVentaController {
 
     @FXML
     void onVolverClick(ActionEvent event) {
-
+    	try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("MenuPrincipal.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			Scene scene = new Scene(root,1300,650);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Menu Principal");
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    	Stage stage = (Stage) btnVolver.getScene().getWindow();
+    	stage.close();
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		contadorCantidad = 1;
+		ObtenerDatos obtenerDatos;
+		try {
+			obtenerDatos = new ObtenerDatos();
+			productos = FXCollections.observableArrayList();
+			productos = obtenerDatos.obtenerProductos();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		this.tblProductos.setItems(productos);	
+		this.colProdNombre.setCellValueFactory(new PropertyValueFactory<Producto, String>("nombre"));
+		this.colProdKilos.setCellValueFactory(new PropertyValueFactory<Producto, Integer>("kilos"));
+		this.colProdStock.setCellValueFactory(new PropertyValueFactory<Producto, Integer>("stock"));
+		
+		try {
+			obtenerDatos = new ObtenerDatos();
+			clientes = FXCollections.observableArrayList();
+			clientes = obtenerDatos.obtenerClientes();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		this.tblClientes.setItems(clientes);
+		
+		this.colClieNombre.setCellValueFactory(new PropertyValueFactory<Cliente, String>("nombre"));
+		this.colClieApellido.setCellValueFactory(new PropertyValueFactory("apellido"));
+		this.colClieDni.setCellValueFactory(new PropertyValueFactory("dni"));
+		this.colClieDireccion.setCellValueFactory(new PropertyValueFactory("direccion"));
+		
+		
+		
+	}
 
 }
