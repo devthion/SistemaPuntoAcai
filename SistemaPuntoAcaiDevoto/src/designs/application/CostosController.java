acc5ed6781270b4e871e1e14192e308c6ad49db5
andycabrera <andycaabrera@users.noreply.github.com>
2020-05-29 15:18:42 -0300

    Conexion a Ver los graficos de las ventas

--- SistemaPuntoAcaiDevoto/src/designs/application/CostosController.java
+++ SistemaPuntoAcaiDevoto/src/designs/application/CostosController.java
@@ -1,9 +1,13 @@
 package application;
 
+import java.util.Objects;
+
+import Alertas.Alerta;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -45,6 +49,11 @@ public class CostosController {
 
     @FXML
     void onGuardarGastoClick(ActionEvent event) {
+    	if(Integer.parseInt(txtMonto.getText()) != 0 ) {
+    		new Alerta().informationAlert("Se ha agregado el Gasto con exito", "Nuevo Gasto");
+    	}else {
+    		new Alerta().errorAlert("Debe ingresar un Monto", "Nuevo Gasto");
+    	}
     	
     }
 
