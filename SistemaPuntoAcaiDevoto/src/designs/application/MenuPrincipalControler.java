8a7b3062b1401a1679e6ed3bf8a8c69528ddb15b
ElDieguii <49466415+ElDieguii@users.noreply.github.com>
2020-05-29 21:28:09 -0300

    arreglo mockito

--- SistemaPuntoAcaiDevoto/src/designs/application/MenuPrincipalControler.java
+++ SistemaPuntoAcaiDevoto/src/designs/application/MenuPrincipalControler.java
@@ -116,7 +116,7 @@ public class MenuPrincipalControler {
     void onEstadisticasClick(ActionEvent event) {
     	try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(getClass().getResource("GraficoVentas.fxml"));
+			loader.setLocation(getClass().getResource("GraficoClientesPorBarrio.fxml"));
 			AnchorPane root = (AnchorPane) loader.load();
 			Scene scene = new Scene(root,1300,650);
 			Stage stage = new Stage();
