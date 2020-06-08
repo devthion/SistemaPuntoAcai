d81b0e33946e08a6a1071dceb5d306f67a6ca1a0
Andy <49444984+andycabrera@users.noreply.github.com>
2020-06-08 11:02:18 -0300

    comit

--- SistemaPuntoAcaiDevoto/src/designs/application/VerClientes.java
+++ SistemaPuntoAcaiDevoto/src/designs/application/VerClientes.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class VerClientes {
