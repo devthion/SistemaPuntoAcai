c1607be48fcbf79080d6fa49ee56631e6bf29743
andycabrera <andrescabrera8199@gmail.com>
2020-07-03 19:17:47 -0300

    Modificacion de las direcciones largas

--- SistemaPuntoAcaiDevoto/src/main/java/application/Main.java
+++ SistemaPuntoAcaiDevoto/src/main/java/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import ConexionBD.ModificarDatos;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -27,6 +28,14 @@ public class Main extends Application {
 	
 	
 	public static void main(String[] args) {
+//		try {
+//			new ModificarDatos().cambiarDirecciones();
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			System.out.println("ERROR");
+//		}
 		launch(args);
+
+		
 	}
 }
